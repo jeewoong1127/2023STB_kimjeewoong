adcf6d5dc05ec64b876f77ae3d06d399409c32ab
kimjeewoong <jwong1127@naver.com>
2023-10-11 16:47:47 +0900

    06. Probability distribution(1)

--- 06. Probability distribution(1).R
+++ 06. Probability distribution(1).R
@@ -6,7 +6,7 @@ dbinom(3,4,0.5)
 pbinom(x, size, prob) 
 #x : 성공의횟수(확률변수x), size : 시행횟수, prob : 성공의확률
 pbinom(1,1000,0.01) 
-pbinom(9,100,0.2)=1-pbinom(90,100,0.8)
+pbinom(9,100,0.2)
 #포아송분포 확률함수
 dpois(x, lambda) 
 #x : 사건의발생횟수, lambda : 평균발생횟수
